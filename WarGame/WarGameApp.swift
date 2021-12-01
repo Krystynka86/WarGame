3337936a2a5a77c0e74d263a52b6a2b5490c88ec
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2021-12-01 15:23:00 +0100

    Initial Commit

---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by Cristina on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
